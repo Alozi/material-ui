d8361786268c1aa70c11dbbec463dc2bdb253b7d
Alozi <alozimozi@gmail.com>
2025-05-26 12:33:33 +0300

    Add custonm theme with provider

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -1,5 +1,38 @@
-import { Box, Grid, Paper, Rating, Typography } from "@mui/material";
+import {
+  createTheme,
+  ThemeProvider,
+  Box,
+  Grid,
+  Paper,
+  Rating,
+  Typography,
+} from "@mui/material";
 import { AccessTime } from "@mui/icons-material";
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: "body2",
+          },
+          style: {
+            fontSize: 11,
+          },
+        },
+        {
+          props: {
+            variant: "body3",
+          },
+          style: {
+            fontSize: 9,
+          },
+        },
+      ],
+    },
+  },
+});
 
 export default function TourCard({
   img,
@@ -10,42 +43,44 @@ export default function TourCard({
   price,
 }) {
   return (
-    <Grid item size={3}>
-      <Paper elevation={3}>
-        <img className="img" src={img} alt="Greece image" />
-        <Box paddingX={1}>
-          <Typography variant="subtitle1" component="h2">
-            {title}
-          </Typography>
-          <Box sx={{ display: "flex", alignItems: "center", gap: 0.5 }}>
-            <AccessTime sx={{ width: 12.5 }} />
-            <Typography variant="body2" component="p">
-              {duration}
+    <ThemeProvider theme={theme}>
+      <Grid item size={3}>
+        <Paper elevation={3}>
+          <img className="img" src={img} alt="Greece image" />
+          <Box paddingX={1}>
+            <Typography variant="subtitle1" component="h2">
+              {title}
             </Typography>
+            <Box sx={{ display: "flex", alignItems: "center", gap: 0.5 }}>
+              <AccessTime sx={{ width: 12.5 }} />
+              <Typography variant="body2" component="p">
+                {duration}
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                gap: 0.5,
+                marginTop: 3,
+              }}
+            >
+              <Rating value={rating} readOnly precision={0.5} size="small" />
+              <Typography variant="body2" component="p">
+                {rating}
+              </Typography>
+              <Typography variant="body3" component="p" marginLeft={2}>
+                {reviews}
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h6" component="h3">
+                {price}
+              </Typography>
+            </Box>
           </Box>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              gap: 0.5,
-              marginTop: 3,
-            }}
-          >
-            <Rating value={rating} readOnly precision={0.5} size="small" />
-            <Typography variant="body2" component="p">
-              {rating}
-            </Typography>
-            <Typography variant="body2" component="p" marginLeft={3}>
-              {reviews}
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" component="h3">
-              {price}
-            </Typography>
-          </Box>
-        </Box>
-      </Paper>
-    </Grid>
+        </Paper>
+      </Grid>
+    </ThemeProvider>
   );
 }
